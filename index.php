e422da4f6b1261b03e1136038453c4f129b533dc
Vuković Nikola <vukovicnikola2014@gmail.com>
2014-04-13 20:51:45 +0100

    bp init

--- index.php
+++ index.php
@@ -1,14 +1,46 @@
 <!doctype html>
-<html lang="en">
+<html class="no-js">
 <head>
-  <meta charset="UTF-8">
-  <title>nikolavukovic.me</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>nv</title>
+    <meta name="description" content="Hello world! This is HTML5 Boilerplate.">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+    <script src="js/vendor/modernizr-2.7.1.min.js"></script>
 </head>
 <body>
-  <h1>
-    <?php 
-      echo "hello there";
-    ?>
-  </h1>
+<!--[if lt IE 8]>
+    <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+<![endif]-->
+
+
+<div class="container-fluid">
+<div class="row">
+    <section class="col col-md-8">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime eaque optio cupiditate earum. Perferendis, quisquam dolor laudantium accusantium velit iste soluta veniam nulla perspiciatis itaque a illum. Dolore, in, iure!</p>
+      <p>Praesentium, ea, voluptatem, repellat explicabo unde hic sequi sunt expedita voluptas quibusdam tenetur sit inventore voluptatum aspernatur dolorum natus quam enim neque eaque ab blanditiis cupiditate obcaecati! Consequatur, non, voluptatem?</p>
+      <p>Nisi, beatae, nobis libero mollitia error iure quasi ab cum dicta unde excepturi dignissimos esse quidem quas nostrum reprehenderit accusamus officia dolorum recusandae placeat quo exercitationem neque ad explicabo quam.</p>
+    </section>
+    <aside class="col col-md-4">
+      <p>Debitis, est, laudantium voluptates necessitatibus provident ipsam quod nostrum eligendi facilis commodi rem reprehenderit sed incidunt neque consequuntur dolor exercitationem perferendis sunt quisquam aliquam! Alias iure minus quod vitae dolorem.</p>
+      <p>At, nam, ad accusamus veritatis quibusdam commodi culpa accusantium velit repellendus sint nemo eius labore inventore iure soluta dicta neque numquam praesentium quisquam sunt. A, vero placeat alias nisi voluptates!</p>
+    </aside>
+</div>
+</div>
+
+
+
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="js/plugins.js"></script>
+<script src="js/main.js"></script>
+
+<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
 </body>
 </html>
+<?php 
+
+exit;
