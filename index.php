f154dd8577715d187457fb85c6fc4cf8094e9d13
Vuković Nikola <vukovicnikola2014@gmail.com>
2014-04-13 18:01:00 +0100

    ch

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
-<?php
-
-// phpinfo();
-
-echo 1;
-
-
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>nikolavukovic.me</title>
+</head>
+<body>
+  <h1>
+    <?php 
+      echo "hello there";
+    ?>
+  </h1>
+</body>
+</html>
